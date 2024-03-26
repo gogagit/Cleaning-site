4a789fbf98d7e75db09f8335b142857f255f9569
Семенютин Илья <ilyasemenyt@gmail.com>
2024-03-26 18:43:53 +0300

    Done

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,12 @@
-
-
+window.addEventListener("load", () =>{
+    const preloader = document.querySelector(".js-preloader")
+    preloader.classList.add("fade-out")
+    setTimeout(() => {
+        preloader.style.display = "none";
+        // animated on scroll
+        AOS.init();
+    }, 600);
+});
 // header bg reveal
 const headerBg = () => {
     const header = document.querySelector(".js-header");
@@ -14,3 +21,22 @@ const headerBg = () => {
     });
 }
 headerBg();
+// nav 
+const navigation = () =>{
+    const navToggler = document.querySelector(".js-nav-toggler");
+    const nav = document.querySelector(".js-nav");
+    const navItems = nav.querySelector("li");
+    const navToggle = () =>{
+        nav.classList.toggle("open");
+        navToggler.classList.toggle("active");
+    }
+    navToggler.addEventListener("click", navToggle);
+    navItems.forEach((li)=>{
+        li.querySelector("a").addEventListener("click",() =>{
+            if(window.innerWidth <= 767){
+                navToggle();
+            } 
+        });
+    });
+}
+navigation();
